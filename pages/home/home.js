006bbc9c7aae9e2dc666370be16adf426b37f99c
alanjoe <1374911647@qq.com>
2020-01-11 11:36:42 +0800

    网络请求、弹框与分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,57 @@
 // pages/home/home.js
 Page({
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      mask: true,
+      success: function () {
+        console.log('展示弹框成功')
+      },
+      fail: function () {
+        console.log('展示弹框失败')
+      },
+      complete: function () {
+        console.log('完成函数的调用')
+      }
+    })
+  },
+  showModal() {
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      // showCancel: false, //去除取消键
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+  showLoading() {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 2000)
+  },
+
+  showActionSheet() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      success(res) {
+        console.log(res.tapIndex)
+      },
+      fail(res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
 
   /**
    * 页面的初始数据
@@ -12,7 +64,28 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 发送网络请求
+    // wx.request({
+    //   url: 'http://jsonplaceholder.typicode.com/posts',
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
 
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: 'post',
+    //   data: {
+    //     name: 'coderwhy',
+    //     age: 18
+    //   },
+    //   success: function(res) {
+    //     console.log(res)
+    //   },
+    //   fail: function(err) {
+    //     console.log(res)
+    //   }
+    // })
   },
 
   /**
@@ -61,6 +134,10 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: '你好啊',
+      path: '/pages/home/home',
+      imageUrl: 'https://via.placeholder.com/600/92c952'
+    }
   }
 })
